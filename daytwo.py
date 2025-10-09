e791a78728c95974282cbf971f0f6d39d5c81b31
Damini Gupta <damini.cs231079@global.org.in>
2025-10-09 15:47:54 +0530

    day2

--- daytwo.py
+++ daytwo.py
@@ -1,13 +1,23 @@
-# x=10,y=3,z=5
-# if(x>y) and (x>z):
-#     print("x is greater")
-# elif (y>z) and (y>x):
-#         print("y is greater")
-# else:
-#         # print("z is greater")
-num = int(input("Enter a number: "))
-sum_digits = 0
-while num > 0:
- sum_digits += num % 10
- num //= 10
-print("Sum of digits is:", sum_digits)
+# # x=10,y=3,z=5
+# # if(x>y) and (x>z):
+# #     print("x is greater")
+# # elif (y>z) and (y>x):
+# #         print("y is greater")
+# # else:
+# #         # print("z is greater")
+# num = int(input("Enter a number: "))
+# sum_digits = 0
+# while num > 0:
+#  sum_digits += num % 10
+#  num //= 10
+# print("Sum of digits is:", sum_digits)
+num=325
+total=0
+count=0
+while num>0:
+    digit=num%10
+    total=total+digit
+    count=count+1
+    num=num//10
+print("Total:", total)
+print("Count:", count)
