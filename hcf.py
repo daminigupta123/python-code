b7363152d4b9665c565c77d9b6b28e8eb3c0a57e
Damini Gupta <damini.cs231079@global.org.in>
2025-10-27 15:44:04 +0530

    hcf

---
+++ hcf.py
@@ -0,0 +1,11 @@
+# find out hcf of two numbers
+def find_hcf(x, y):
+    if x > y:
+        smaller = y
+    else:
+        smaller = x
+    for i in range(1, smaller + 1):
+        if (x % i == 0) and (y % i == 0):
+            hcf = i
+    return hcf
+print(find_hcf(12, 18))
