845ba5bd095f97037e8bfbec204b4d8d03331be9
Damini Gupta <damini.cs231079@global.org.in>
2025-11-02 21:09:49 +0530

    pascal tringles

---
+++ p15.py
@@ -0,0 +1,12 @@
+
+def pascal_triangle(n):
+    for i in range(n):
+        num = 1
+        for j in range(i + 1):
+            print(num, end=" ")
+            num = num * (i - j) // (j + 1)
+        print()  
+
+rows = int(input("Enter number of rows: "))
+
+pascal_triangle(rows)
