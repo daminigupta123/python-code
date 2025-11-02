cab31815841061a1b9c2dfe4b284a55b72374d76
Damini Gupta <damini.cs231079@global.org.in>
2025-11-02 18:53:14 +0530

    p10

---
+++ p10.py
@@ -0,0 +1,27 @@
+def find_partners():
+    try:
+      
+        N = int(input())
+        X = int(input())
+for i in range(N):
+                Y = int(input())
+
+                if Y >= X:
+                        print("YES")
+                        else:
+                print("NO")
+                
+                except EOFError:
+    pass
+    except ValueError:
+     
+    print("Invalid input format.")
+       print("Example Output:")
+print("YES")
+
+print("YES")
+
+print("NO")
+print("YES")
+
+print("NO")
