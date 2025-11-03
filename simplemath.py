33743cb7481ea3e455d399d41e6cafecf3847f02
Damini Gupta <damini.cs231079@global.org.in>
2025-11-03 12:51:45 +0530

    module

---
+++ simplemath.py
@@ -0,0 +1,5 @@
+x=8888
+def add(a, b):
+    print("the sum",a+b)
+def product(a, b):
+        print("the product",a*b)
---
+++ test.py
@@ -0,0 +1,4 @@
+import simplemath
+print(simplemath.x)
+simplemath.add(5, 10)
+simplemath.product(5, 10)
