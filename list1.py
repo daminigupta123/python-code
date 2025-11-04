e74924f8ccdc9a2cf49598c6bbc089879d28ae4a
Damini Gupta <damini.cs231079@global.org.in>
2025-11-04 10:42:08 +0530

    print list

---
+++ list1.py
@@ -0,0 +1,10 @@
+emptylist=[]  #ye khali rhegi 
+lst=['one','two','three'] #list of strings
+lst2=[1,2,3,4,5] #list of integers
+lst3=[[1,2],[3,4],[5,6]] #list of lists
+print(emptylist)
+print(lst)
+print(lst2)
+print(lst3)
+
+ 
--- palindromelemda.py
+++ palindromelemda.py
@@ -1,10 +1,29 @@
-def is_palindrome(n):
-    curr = n
-    rev = 0
-    while curr > 0:
-        rev = rev * 10 + curr % 10
-        curr //= 10
-    return n == rev
+# def is_palindrome(n):
+#     curr = n
+#     rev = 0
+#     while curr > 0:
+#         rev = rev * 10 + curr % 10
+#         curr //= 10
+#     return n == rev
 
+# num = int(input("Enter a number: "))
+# print("Palindrome" if is_palindrome(num) else "Not Palindrome")
+# ✅ Best understandable version (no str, clean)
+is_palindrome = lambda n: (
+    (lambda temp, rev=0: (
+        (lambda: True if temp == rev else False)()
+   
+   
+   
+   
+    )
+    )
+)
+   
+   
+
+
+# Example
 num = int(input("Enter a number: "))
 print("Palindrome" if is_palindrome(num) else "Not Palindrome")
+
