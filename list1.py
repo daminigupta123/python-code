8d813a09057ee2f91539fbd96a3ac2680a406668
Damini Gupta <damini.cs231079@global.org.in>
2025-11-04 11:00:15 +0530

    list concepte

--- list1.py
+++ list1.py
@@ -2,9 +2,14 @@ emptylist=[]  #ye khali rhegi
 lst=['one','two','three'] #list of strings
 lst2=[1,2,3,4,5] #list of integers
 lst3=[[1,2],[3,4],[5,6]] #list of lists
+lst4=['one',2,[3,4],5.6] #list of mixed data types
 print(emptylist)
 print(lst)
 print(lst2)
 print(lst3)
-
- 
+print(lst4)
+print(len(lst3)) #length of list
+lst.append('four') #adding element at the end
+print(lst)
+lst.insert(1,'one and half') #adding element at specific index
+print(lst)
