09a6667024f1f33f74ee6c4dfb8fadc46e27611e
Damini Gupta <damini.cs231079@global.org.in>
2025-11-12 14:45:05 +0530

    list

--- list1.py
+++ list1.py
@@ -1,15 +1,49 @@
-emptylist=[]  #ye khali rhegi 
-lst=['one','two','three'] #list of strings
-lst2=[1,2,3,4,5] #list of integers
-lst3=[[1,2],[3,4],[5,6]] #list of lists
-lst4=['one',2,[3,4],5.6] #list of mixed data types
-print(emptylist)
+# emptylist=[]  #ye khali rhegi 
+# lst=['one','two','three'] #list of strings
+# lst2=[1,2,3,4,5] #list of integers
+# lst3=[[1,2],[3,4],[5,6]] #list of lists
+# lst4=['one',2,[3,4],5.6] #list of mixed data types
+# print(emptylist)
+# print(lst)
+# print(lst2)
+# print(lst3)
+# print(lst4)
+# print(len(lst3)) #length of list
+# lst.append('four') #adding element at the end
+# print(lst)
+# lst.insert(1,'one and half') #adding element at specific index
+# print(lst)
+# lst.remove('two') #removing specific element
+# print(lst)
+lst = ['one', 'two', 'three', 'four', 'five']
+lst2=['six','seven']
+lst.extend(['six', 'seven']) 
+
+ # adding multiple elements at the end
+# del lst[2]  # removing element at index 2
+# print(lst)
+# a=lst.pop()  # removing and returning last element
+# print(a)
+# print(lst)
+# if 'two' in lst:
+# 
+    # print("two is present in the list")
+# if 'six' not in lst:
+    # print("six is not present in the list")
+# lst.reverse()  # reversing the list
+# print(lst)
+numbers = [5, 2, 9, 1, 5, 6]
+sorted_lst = numbers.sort()  # sorting the list in ascending order
+print(numbers)
+lst=[1,20,'b',40,'a',10]
+lst.sort()
 print(lst)
-print(lst2)
-print(lst3)
-print(lst4)
-print(len(lst3)) #length of list
-lst.append('four') #adding element at the end
-print(lst)
-lst.insert(1,'one and half') #adding element at specific index
-print(lst)
+
+
+
+
+
+
+
+
+
