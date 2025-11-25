9e51c45ca76d4d43f6338138d80c3fccfcf99a95
Damini Gupta <damini.cs231079@global.org.in>
2025-11-25 15:35:20 +0530

    dictonery  conetent

--- dictonery.py
+++ dictonery.py
@@ -1,8 +1,14 @@
 # empty dictonery
-my_dict={}
+# my_dict={}
 # dictnry with integer keys
-my_dict={1:'abd',2:'xyz'}
-print(my_dict)
+# my_dict={1:'abd',2:'xyz'}
+# print(my_dict)
 # dict with mixed keys
-my_dict={'name':"damini",2:['abc','xyz']}
+# my_dict={'name':"damini",2:['abc','xyz']}
+# print(my_dict)
+my_dict=dict()
+my_dict=dict([(5,'abc'),(8,'damini')])
+print(my_dict)
+my_dict={'name':'damini','age':19,'adress':'njngf'}
+my_dict['degree']='btech'
 print(my_dict)
