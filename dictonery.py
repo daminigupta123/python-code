316d5d05a11e01f28b23d18e12741808b16ae9c7
Damini Gupta <damini.cs231079@global.org.in>
2025-11-25 15:59:09 +0530

    dictnery

--- dictonery.py
+++ dictonery.py
@@ -6,9 +6,24 @@
 # dict with mixed keys
 # my_dict={'name':"damini",2:['abc','xyz']}
 # print(my_dict)
-my_dict=dict()
-my_dict=dict([(5,'abc'),(8,'damini')])
-print(my_dict)
-my_dict={'name':'damini','age':19,'adress':'njngf'}
-my_dict['degree']='btech'
-print(my_dict)
+# my_dict=dict()
+# my_dict=dict([(5,'abc'),(8,'damini')])
+# print(my_dict)
+# my_dict={'name':'damini','age':19,'adress':'njngf'}
+# my_dict['degree']='btech'
+# print(my_dict)
+# print(my_dict.pop('age'))
+# print(my_dict)
+#remove anarbetery keys
+# my_dict.popitem()
+# print(my_dict)
+sqaures={2:4,3:5,4:54}
+del sqaures[2]
+print(sqaures)
+subjects={}.fromkeys(['math','english'],0)
+print(subjects)
+subjects={2:4,4:9,3:9}
+print(subjects.values())
+print(subjects.items())
+d={}
+print(dir(d))
