6ab4d6799ea86f5607f9ff1d7110a38e8be58c02
Damini Gupta <damini.cs231079@global.org.in>
2025-11-25 15:20:13 +0530

    dictonery item

---
+++ dictonery.py
@@ -0,0 +1,8 @@
+# empty dictonery
+my_dict={}
+# dictnry with integer keys
+my_dict={1:'abd',2:'xyz'}
+print(my_dict)
+# dict with mixed keys
+my_dict={'name':"damini",2:['abc','xyz']}
+print(my_dict)
