2c2b009261afd8db1513cabc8272c6388000b12b
Damini Gupta <damini.cs231079@global.org.in>
2025-11-25 16:13:20 +0530

    dictonery comprihension

--- dict_comprehenson.py
+++ dict_comprehenson.py
@@ -1,3 +1,6 @@
-d={'a':1,'b':2,'c':3}
-for pair in d.items():
-    print(pair)
+# d={'a':1,'b':2,'c':3}
+# for pair in d.items():
+    # print(pair )
+d={'a':2,'b':2,'c':3}
+d={k + 'c':v*2 for k,v in d.items() if v>2}
+print(d)
