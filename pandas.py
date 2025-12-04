6cc6d7d50761d6b0f4faec6b73a78e2bc65a6d39
Damini Gupta <damini.cs231079@global.org.in>
2025-12-04 16:23:03 +0530

    pandas or numpy

--- pandas.py
+++ pandas.py
@@ -1,4 +1,9 @@
-import pandas as pd 
+import pandas as pd
+import numpy as np 
 df=pd.read_csv('nyc_weather.csv')
 print(df)
 
+min_science = df['science_original'].min()
+max_science = df['science_original'].max()
+grade_boundries=np.linspace(min_science, max_science, 6)
+print(grade_boundries)
