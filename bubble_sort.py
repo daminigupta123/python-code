4c7a8f686cd0a476dd55493051de88aad6f48372
Damini Gupta <damini.cs231079@global.org.in>
2025-12-11 21:43:00 +0530

    bubble sort version

---
+++ bubble_sort.py
@@ -0,0 +1,8 @@
+N = int(input())
+A = list(map(int, input().split()))
+for i in range(N):
+    for j in range(0, N - i - 1):
+        if A[j] > A[j + 1]:
+            A[j], A[j + 1] = A[j + 1], A[j]
+for x in A:
+    print(x, end=" ")
