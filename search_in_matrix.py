0682fe8c3821fe8384cc20983eca5039786842f0
Damini Gupta <damini.cs231079@global.org.in>
2025-12-12 20:06:00 +0530

    search in matrix of numbers

--- search_in_matrix.py
+++ search_in_matrix.py
@@ -1,16 +1,32 @@
 
+# N, M = map(int, input().split())
+
+# found = False
+
+# for _ in range(N):
+#     row = list(map(int, input().split()))
+#     if X in row:
+#         found = True
+
+# X = int(input())
+
+
+# if found:
+#     print("will not take number")
+# else:
+#     print("will take number")
+# Read N and M
 N, M = map(int, input().split())
 
-found = False
-
+matrix = []
 for _ in range(N):
-    row = list(map(int, input().split()))
+    matrix.append(list(map(int, input().split())))
+X = int(input())
+found = False
+for row in matrix:
     if X in row:
         found = True
-
-X = int(input())
-
-
+        break
 if found:
     print("will not take number")
 else:
